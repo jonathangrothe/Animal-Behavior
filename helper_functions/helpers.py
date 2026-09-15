6b1a8ad1c57fcfccbf89491cfaa6bb6126139de4
Jonathan Grothe <grothejonathan@gmail.com>
2026-09-15 13:26:58 -0700

    deleted that weird get_estimated_sigma function and updated helpers docs

--- helper_functions/helpers.py
+++ helper_functions/helpers.py
@@ -1,7 +1,20 @@
+'''
+A few helper functions that took more lines of code then I wanted to put in the other files
+'''
 import numpy as np
 import math
 
 def get_estimated_angle(a1,a2):
+    # Mostly a relic of the bifurcation angle saga
+    '''
+    A function that takes the angles of two lines and gets the estimated bifurcation angle,
+    Is meant to be able to handle negatives and angles that are more than 2pi away from each other
+    Parameters:
+    a1: the angle of line 1 (radians)
+    a2: the angle of line 2 (radians)
+    Returns:
+    aprox_angle: the approximate bifurcation angle
+    '''
     qa1 = 1
     qa2 = 1    
     adj_a1 = a1
@@ -60,51 +73,6 @@ def get_estimated_angle(a1,a2):
 
     return aprox_angle
 
-def get_estimated_sigma(p0,p1,p2,h0,h1,h2,N):
-    # we assume we are at p0 and want to find how attractive the aggregated bump between p1 and p2 is
-    x = p0[0]
-    y = p0[1]
-    x1 = p1[0]
-    y1 = p1[1]
-    x2 = p2[0]
-    y2 = p2[1]
-    angle_1 = np.atan2(y1-y,x1-x)
-    angle_2 = np.atan2(y2-y,x2-x)
-    theor_neuron1 = angle_1*N/(2*np.pi)
-    theor_neuron2 = angle_2*N/(2*np.pi)
-    delta_ang1 =  np.abs(theor_neuron1-round(theor_neuron1))
-    delta_ang2 = np.abs(theor_neuron2-round(theor_neuron2))
-
-    for item in range(int(theor_neuron2),math.ceil(theor_neuron1)):
-        # calculate the theoretical sigma needed for the total attraction at this neuron to be greater than the total attraction at the other neuron
-        # Note: we assume that we are at p0, but in reality we are almost there and if sigma is small then we are pretty sensitive to where we are in the ring attractor angle-wise,
-        # ie: if the angle to p0 lands at neuron 30.5, then that is a much lower attraction than if it was at neuron 30 (but also we need to account for total area under the curve...)
-        # to account for total area under the curve, we should probably just do the same thing (but it will be much more complicated to calculate sigma...)
-        # Come back to this math, I think its not quite right
-        neuron_angle = item*2*np.pi/100
-        if neuron_angle > np.pi:
-            neuron_angle -= 2*np.pi
-        delta_ang1 = np.abs(angle_1-neuron_angle)
-        delta_ang2 = np.abs(angle_2-neuron_angle)
-        log_h0 = math.log(h0)
-        log_h1 = math.log(h1)
-        log_h2 = math.log(h2)
-        print(f"log h0: {log_h0}, log h1: {log_h1}, log h2: {log_h2}, delta ang1: {delta_ang1}, delta ang2: {delta_ang2}, num: {-0.5*(delta_ang1**2+delta_ang2**2)}, denom: {log_h0-log_h1-log_h2}")
-        sigma_est = np.sqrt((0.5*(delta_ang1**2+delta_ang2**2))/(log_h0-log_h1-log_h2))
-        print(f"angle: {neuron_angle}, neuron: {item}, sigma: {sigma_est}")
-
-    theor_ang_delta = np.abs(angle_1-angle_2)
-    j = np.log(h0/(h1+h2))
-    print(f"assuming delta angles are equal: j: {j}, delta: {theor_ang_delta/2}")
-    theor_sigma = np.sqrt((-0.5*(theor_ang_delta/2)**2)/j) # instead of just calculating it at the theor_ang_delta, we need to calculate it everywhere
-    print(f"theoretical sigma: {theor_sigma}, difference from neuron and true angles: 1: {delta_ang1}, 2: {delta_ang2}")
-    return theor_sigma
-
-
-# new function: goal: given the geometry and a target of interest, calculate the lowest possible angle between targets in the region it could feasibly traverse?
-# ie: if we have a target at pi and some number of targets such that if all are even the aggregate is pi/2, and we want it to move towards pi, then as we increase it will
-# theoretically travel from taking that 'default trajectory' towards eventually taking a direct path to our target at pi. What we need to conisder is not just the difference, but
-# how that difference will effect the agents initial trajectory, which will effect the target it reaches. 
 
 def trajectory_grid(xpoints,ypoints,N,agg=1,indi=0):
     '''
@@ -116,6 +84,8 @@ def trajectory_grid(xpoints,ypoints,N,agg=1,indi=0):
         N: the number of points per side
         agg: the proportion for the completely aggregated solution
         indi: the proportion for the nearest target solution
+    returns:
+        angles: a list of size 101*101 with the angles for the vector field arranged from (0,0) -> (100,0) -> ... -> (100,100)
     '''
     
     angles = []
